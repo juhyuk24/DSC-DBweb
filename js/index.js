6c221787bb7ea52009c77ba827074542d1f18be9
방주혁 <102583124+juhyuk24@users.noreply.github.com>
2023-07-12 17:29:58 +0900

    modify UI

--- js/index.js
+++
@@ -1,5 +0,0 @@
-function patchinnerHTML() {
-    var divHTML = document.getElementsByClassName("inner-space");
-    divHTML.innerHTML = "<div href='index.html'> dfd</div>";
-    console.log(divHTML.innerHTML);
-}
