d8d745b10d2c024394eb7ffd1fb5ed57a0ffb3e1
방주혁 <102583124+juhyuk24@users.noreply.github.com>
2023-07-21 17:48:40 +0900

    update pages

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -2,39 +2,14 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-function number_format(number, decimals, dec_point, thousands_sep) {
-  // *     example: number_format(1234.56, 2, ',', ' ');
-  // *     return: '1 234,56'
-  number = (number + '').replace(',', '').replace(' ', '');
-  var n = !isFinite(+number) ? 0 : +number,
-    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-    s = '',
-    toFixedFix = function(n, prec) {
-      var k = Math.pow(10, prec);
-      return '' + Math.round(n * k) / k;
-    };
-  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-  if (s[0].length > 3) {
-    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-  }
-  if ((s[1] || '').length < prec) {
-    s[1] = s[1] || '';
-    s[1] += new Array(prec - s[1].length + 1).join('0');
-  }
-  return s.join(dec);
-}
 
-// Area Chart Example
 var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
-      label: "Earnings",
+      fill: 'start',
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
@@ -46,7 +21,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [0, 10, 50, 15, 10, 20, 15, 25, 20, 30, 25, 40],
     }],
   },
   options: {
@@ -65,21 +40,17 @@ var myLineChart = new Chart(ctx, {
           unit: 'date'
         },
         gridLines: {
-          display: false,
-          drawBorder: false
+          display: true,
+          drawBorder: true
         },
         ticks: {
-          maxTicksLimit: 7
+          maxTicksLimit: 20
         }
       }],
       yAxes: [{
         ticks: {
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return '$' + number_format(value);
-          }
+          maxTicksLimit: 10,
+          padding: 10
         },
         gridLines: {
           color: "rgb(234, 236, 244)",
@@ -106,29 +77,24 @@ var myLineChart = new Chart(ctx, {
       displayColors: false,
       intersect: false,
       mode: 'index',
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-        }
-      }
+      caretPadding: 10
     }
   }
 });
 
+var i = 1;
 setInterval(() => {
   const newData = Math.floor(Math.random() * 100);
-  const dataValues5 = myLineChart.data.datasets.data;
-  const labels5 = myLineChart.data.labels;
+  const dataValues = myLineChart.data.datasets[0].data;
+  const labels = myLineChart.data.labels;
 
-  dataValues5.shift();
-  labels5.shift();
+  dataValues.shift();
+  labels.shift();
 
-  dataValues5.push(newData);
-  labels5.push(labels5[labels5.length - 1] + 1);
+  dataValues.push(newData);
+  labels.push(i++);
 
-  myLineChart.data.labels = labels5;
-  myLineChart.data.datasets[0].data = dataValues5;
+  myLineChart.data.labels = labels;
+  myLineChart.data.datasets[0].data = dataValues;
   myLineChart.update();
 }, 1000);
