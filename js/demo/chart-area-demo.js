12f35a70a1592d15dc2dbaf913e4afa6c557e607
방주혁 <102583124+juhyuk24@users.noreply.github.com>
2023-07-31 15:24:21 +0900

    update pages

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -5,96 +5,122 @@ Chart.defaults.global.defaultFontColor = '#858796';
 
 var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-    datasets: [{
-      fill: 'start',
-      lineTension: 0.3,
-      backgroundColor: "rgba(78, 115, 223, 0.05)",
-      borderColor: "rgba(78, 115, 223, 1)",
-      pointRadius: 3,
-      pointBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointBorderColor: "rgba(78, 115, 223, 1)",
-      pointHoverRadius: 3,
-      pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
-      pointHoverBorderColor: "rgba(78, 115, 223, 1)",
-      pointHitRadius: 10,
-      pointBorderWidth: 2,
-      data: [0, 10, 50, 15, 10, 20, 15, 25, 20, 30, 25, 40],
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
+    type: 'line',
+    data: {
+        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+        datasets: [{
+            fill: 'start',
+            lineTension: 0.3,
+            backgroundColor: "rgba(78, 115, 223, 0.05)",
+            borderColor: "rgba(78, 115, 223, 1)",
+            pointRadius: 3,
+            pointBackgroundColor: "rgba(78, 115, 223, 1)",
+            pointBorderColor: "rgba(78, 115, 223, 1)",
+            pointHoverRadius: 3,
+            pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+            pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+            pointHitRadius: 10,
+            pointBorderWidth: 2,
+            data: [0, 10, 50, 15, 10, 20, 15, 25, 20, 30, 25, 40],
+        }],
     },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
+    options: {
+        maintainAspectRatio: false,
+        layout: {
+            padding: {
+                left: 10,
+                right: 25,
+                top: 25,
+                bottom: 0
+            }
         },
-        gridLines: {
-          display: true,
-          drawBorder: true
+        scales: {
+            xAxes: [{
+                time: {
+                    unit: 'date'
+                },
+                gridLines: {
+                    display: true,
+                    drawBorder: true
+                },
+                ticks: {
+                    maxTicksLimit: 20
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    maxTicksLimit: 10,
+                    padding: 10
+                },
+                gridLines: {
+                    color: "rgb(234, 236, 244)",
+                    zeroLineColor: "rgb(234, 236, 244)",
+                    drawBorder: false,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                }
+            }],
         },
-        ticks: {
-          maxTicksLimit: 20
+        legend: {
+            display: false
+        },
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            intersect: false,
+            mode: 'index',
+            caretPadding: 10
         }
-      }],
-      yAxes: [{
-        ticks: {
-          maxTicksLimit: 10,
-          padding: 10
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      intersect: false,
-      mode: 'index',
-      caretPadding: 10
     }
-  }
 });
 
-var i = 13;
-setInterval(() => {
-  const newData = Math.floor(Math.random() * 100);
-  const dataValues = myLineChart.data.datasets[0].data;
-  const labels = myLineChart.data.labels;
+let i = 13;
+let intervalID;
+let intervalTime = document.getElementById('setInterval').value * 1000;
 
-  dataValues.shift();
-  labels.shift();
+intervalID = setInterval(() => {
+    const newData = Math.floor(Math.random() * 100);
+    const dataValues = myLineChart.data.datasets[0].data;
+    const labels = myLineChart.data.labels;
 
-  dataValues.push(newData);
-  labels.push(i++);
+    dataValues.shift();
+    labels.shift();
 
-  myLineChart.data.labels = labels;
-  myLineChart.data.datasets[0].data = dataValues;
-  myLineChart.update();
-}, 1000);
+    dataValues.push(newData);
+    labels.push(i++);
+
+    myLineChart.data.labels = labels;
+    myLineChart.data.datasets[0].data = dataValues;
+    myLineChart.update();
+}, intervalTime);
+
+function changeInterval() {
+    intervalTime = document.getElementById('setInterval').value * 1000;
+    if(intervalTime >= 1) {
+        clearInterval(intervalID);
+        intervalID = setInterval(() => {
+            const newData = Math.floor(Math.random() * 100);
+            const dataValues = myLineChart.data.datasets[0].data;
+            const labels = myLineChart.data.labels;
+
+            dataValues.shift();
+            labels.shift();
+
+            dataValues.push(newData);
+            labels.push(i++);
+
+            myLineChart.data.labels = labels;
+            myLineChart.data.datasets[0].data = dataValues;
+            myLineChart.update();
+        }, intervalTime);
+    }
+    else{ }
+}
--- js/demo/chart-bar-demo.js
+++ js/demo/chart-bar-demo.js
@@ -4,77 +4,77 @@ Chart.defaults.global.defaultFontColor = '#858796';
 
 var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["DM 데이터베이스", "TM 데이터베이스", "SI 데이터베이스", "MS 데이터베이스"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "#4e73df",
-      hoverBackgroundColor: "#2e59d9",
-      borderColor: "#4e73df",
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
+    type: 'bar',
+    data: {
+        labels: ["dmdb", "tmdb", "sidb", "msdb"],
+        datasets: [{
+            label: "Revenue",
+            backgroundColor: "#4e73df",
+            hoverBackgroundColor: "#2e59d9",
+            borderColor: "#4e73df",
+        }],
     },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
+    options: {
+        maintainAspectRatio: false,
+        layout: {
+            padding: {
+                left: 10,
+                right: 25,
+                top: 25,
+                bottom: 0
+            }
         },
-        gridLines: {
-          display: false,
-          drawBorder: false
+        scales: {
+            xAxes: [{
+                time: {
+                    unit: 'month'
+                },
+                gridLines: {
+                    display: false,
+                    drawBorder: false
+                },
+                ticks: {
+                    maxTicksLimit: 6
+                },
+                maxBarThickness: 25,
+            }],
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 100,
+                    maxTicksLimit: 5,
+                    padding: 10,
+                },
+                gridLines: {
+                    color: "rgb(234, 236, 244)",
+                    zeroLineColor: "rgb(234, 236, 244)",
+                    drawBorder: false,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                }
+            }],
         },
-        ticks: {
-          maxTicksLimit: 6
+        legend: {
+            display: false
         },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 100,
-          maxTicksLimit: 5,
-          padding: 10,
+        tooltips: {
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
         },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
-  }
+    }
 });
 
 setInterval(() => {
-  const newData = [getRandomNumber(30, 50), getRandomNumber(20, 30), getRandomNumber(70, 80), getRandomNumber(40, 45)];
-  myBarChart.data.datasets[0].data = newData;
-  myBarChart.update();
+    const newData = [getRandomNumber(30, 50), getRandomNumber(20, 30), getRandomNumber(70, 80), getRandomNumber(40, 45)];
+    myBarChart.data.datasets[0].data = newData;
+    myBarChart.update();
 }, 1000);
